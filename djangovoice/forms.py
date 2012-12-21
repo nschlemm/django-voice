e6003cf0ce8c1810bd283ef0a148067098ff7e0b
Gokmen Gorgen <gokmen@alageek.com>
2012-12-21 03:32:29 +0200

    submit feedback page style update.

--- djangovoice/forms.py
+++ djangovoice/forms.py
@@ -1,14 +1,21 @@
 from django import forms
-from django.forms import ModelForm
 from djangovoice.models import Feedback
 
 
-class WidgetForm(ModelForm):
+class WidgetForm(forms.ModelForm):
     class Meta:
         model = Feedback
-        fields = ('email', 'type', 'anonymous', 'private', 'title', 'description',)
+        fields = (
+            'email', 'type', 'anonymous', 'private', 'title', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(WidgetForm, self).__init__(*args, **kwargs)
+
+        for field_name in ['title', 'description']:
+            field = self.fields[field_name]
+            field.widget.attrs = {'class': 'input-block-level'}
 
 
-class EditForm(ModelForm):
+class EditForm(forms.ModelForm):
     class Meta:
         model = Feedback
