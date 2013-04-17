331a30585424092c1c8c4244154f78bceea08a97
Gokmen Gorgen <gokmen@alageek.com>
2013-04-17 23:10:39 +0300

    improve admin frontend of Feedback.

--- djangovoice/admin.py
+++ djangovoice/admin.py
@@ -3,8 +3,16 @@ from djangovoice.models import Feedback, Status, Type
 
 
 class SlugFieldAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
+    prepopulated_fields = {'slug': ('title',)}
 
 
-admin.site.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = [
+        '__unicode__', 'type', 'status', 'duplicate', 'anonymous', 'private',
+        'user', 'email']
+    list_filter = ['type', 'status', 'anonymous', 'private']
+    list_editable = ['type', 'status', 'anonymous', 'private']
+
+
+admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register([Status, Type], SlugFieldAdmin)
