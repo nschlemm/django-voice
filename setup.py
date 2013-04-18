53c52487a934bb11012630758ccf38193f05b442
Gokmen Gorgen <gokmen@alageek.com>
2013-04-18 11:37:45 +0300

    minimum django version will be 1.4

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
+import os.path
 from setuptools import setup, find_packages
 
-djangovoice = __import__('djangovoice')
-description = file('README.rst', 'r')
+pkg_name = 'djangovoice'
+version = __import__(pkg_name).__version__
+description = file('README.rst', 'r').read()
+
+# get requires from requirements/global.txt file.
+requires_file_name = os.path.join(
+    os.path.dirname(__file__), 'requirements', 'global.txt')
+with file(requires_file_name) as install_requires:
+    install_requires = map(lambda x: x.split(), install_requires.readlines())
 
 setup(
     name='django-voice',
-    version=djangovoice.get_version(),
-    description="A feedback application for Django 1.3 or later",
-    long_description=description.read(),
+    version=version,
+    description="A feedback application for Django",
+    long_description=description,
     author=u'Gökmen Görgen',
     author_email='gokmen@alageek.com',
     url='https://github.com/gkmngrgn/django-voice',
@@ -17,14 +25,11 @@ setup(
     packages=find_packages(exclude=('demo', 'demo.*')),
     include_package_data=True,
     zip_safe=False,
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",
         "Environment :: Web Environment"
-    ],
-    install_requires=[
-        "Django>=1.3",
-        "qhonuskan-votes>=0.1.4"
     ]
 )
